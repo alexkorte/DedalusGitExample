1258a32da521611d6e126736229c4afccd15c756
Alexander Kortenbreer <alexander.kortenbreer@web.de>
2025-05-02 09:35:05 +0200

    added Calculator

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,12 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Init Calculator");
+        Calculator calculator = new Calculator();
+        int result = calculator.add(1, 2);
+        System.out.println("Result: " + result);
     }
+
+
 }
+
